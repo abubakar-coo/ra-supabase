4bd163361da425a451b5489efed254c9d8dc112d
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-07-30 14:56:35 +0200

    add mfa routes to admin guesser

--- packages/ra-supabase/src/AdminGuesser.tsx
+++ packages/ra-supabase/src/AdminGuesser.tsx
@@ -15,6 +15,9 @@ import {
     LoginPage,
     SetPasswordPage,
     ForgotPasswordPage,
+    MFAEnrollPage,
+    MFAChallengePage,
+    MFAUnenrollPage,
 } from 'ra-supabase-ui-materialui';
 import { createClient } from '@supabase/supabase-js';
 import { defaultI18nProvider } from './defaultI18nProvider';
@@ -57,7 +60,9 @@ export const AdminGuesser = (
             : undefined;
     const defaultAuthProvider =
         instanceUrl && apiKey && defaultSupabaseClient
-            ? supabaseAuthProvider(defaultSupabaseClient, {})
+            ? supabaseAuthProvider(defaultSupabaseClient, {
+                  enforceMFA: true, // TODO remove (for test purposes only)
+              })
             : undefined;
 
     return (
@@ -101,6 +106,9 @@ import {
     ForgotPasswordPage,
     ListGuesser,
     LoginPage,
+    MFAEnrollPage,
+    MFAChallengePage,
+    MFAUnenrollPage,
     SetPasswordPage,
     ShowGuesser,
     defaultI18nProvider,
@@ -134,6 +142,9 @@ export const App = () => (
             <CustomRoutes noLayout>
                 <Route path={SetPasswordPage.path} element={<SetPasswordPage />} />
                 <Route path={ForgotPasswordPage.path} element={<ForgotPasswordPage />} />
+                <Route path={MFAEnrollPage.path} element={<MFAEnrollPage />} />
+                <Route path={MFAChallengePage.path} element={<MFAChallengePage />} />
+                <Route path={MFAUnenrollPage.path} element={<MFAUnenrollPage />} />
             </CustomRoutes>
         </Admin>
     </BrowserRouter>
@@ -169,6 +180,15 @@ export const App = () => (
                 <Route
                     path={ForgotPasswordPage.path}
                     element={<ForgotPasswordPage />}
+                />
+                <Route path={MFAEnrollPage.path} element={<MFAEnrollPage />} />
+                <Route
+                    path={MFAChallengePage.path}
+                    element={<MFAChallengePage />}
+                />
+                <Route
+                    path={MFAUnenrollPage.path}
+                    element={<MFAUnenrollPage />}
                 />
             </CustomRoutes>
         </AdminUI>
